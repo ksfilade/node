8ef159f446b973b7ae19d28e8f5a9d4f4f52cd81
kirilstojanovski <kiril@artistika.com>
2019-02-17 22:46:18 +0100

    initial

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const hbs=require('hbs')
+var app=express();
+
+app.set('view engine','hbs')
+app.use(express.static(__dirname+"/public"))
+
+app.use((req,res,next)=>{
+var now=new Date().toString();
+
+  console.log(`${now}:`)
+  next();
+})
+
+app.get('/',(req,res)=>{
+res.send({name:"kiril",age:10});
+});
+app.get('/about',(req,res)=>{
+  res.render('about.hbs');
+})
+app.listen(3000);
